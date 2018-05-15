edcfa7a6136c8ef010a92e915aba0de3503e154d
koopdev <koop@koop.codes>
2018-05-14 22:45:32 -0700

    Begin Checkpoint 2 - Room List

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import * as firebase from "firebase";
+import RoomList from "./components/RoomList.js";
+
+var config = {
+  apiKey: "AIzaSyAKGIi6kUifZBMmKS5C3b7zt0d9FirZO1k",
+  authDomain: "koop-chat.firebaseapp.com",
+  databaseURL: "https://koop-chat.firebaseio.com",
+  projectId: "koop-chat",
+  storageBucket: "koop-chat.appspot.com",
+  messagingSenderId: "337124092066",
+};
+firebase.initializeApp(config);
+
+this.state = {
+  rooms: [],
+};
+
+this.roomsRef = this.props.firebase.database().ref("rooms");
 
 class App extends Component {
   render() {
@@ -8,11 +26,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Koop Chat</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <span id="main">
+          <p>
+            linke to RoomList Route
+            <Route path="/roomlist" component={RoomList} />
+          </p>
+        </span>
       </div>
     );
   }
---
+++ src/components/RoomList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export class RoomList extends React.Component {
+	render () {
+		return (
+			<div className="RoomList">Room List</div>
+		);
+	}
+}
