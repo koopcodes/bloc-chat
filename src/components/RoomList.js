fa6d79fab92a4af8ff7f0140ef9f36d95235cb69
koopdev <koop@koop.codes>
2018-05-17 14:53:16 -0700

    Checkpoint 3 - Create Room COMPLETE

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
-import './RoomList.css';
+import "./RoomList.css";
 
 class RoomList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       rooms: [],
+      newRoomName: "",
     };
     this.roomsRef = this.props.firebase.database().ref("rooms");
+    this.addNewRoom = this.addNewRoom.bind(this);
+  }
+
+  addNewRoom(e) {
+    e.preventDefault();
+    if (this.validateRoomName(this.state.newRoomName)) {
+      this.roomsRef.push({ name: this.state.newRoomName });
+      console.log("New Room Added!");
+      this.setState({ newRoomName: "" });
+    }
   }
 
   componentDidMount() {
@@ -15,27 +26,41 @@ class RoomList extends Component {
       console.log(snapshot);
       const room = snapshot.val();
       room.key = snapshot.key;
-      this.setState({ rooms: this.state.rooms.concat(room) })
+      this.setState({ rooms: this.state.rooms.concat(room) });
     });
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    this.setState({ newRoomName: e.target.value });
+  }
+
+  validateRoomName(newRoomName) {
+    const newRoomLength = newRoomName.trim().length;
+    if (newRoomLength > 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   render() {
     return (
       <section id="room-component">
         <ul id="room-list">
-          {this.state.rooms.map((data, index) => (
-            <li key={index} className="room-name">
-			        {data.name}
+          {this.state.rooms.map((room, index) => (
+            <li key={index} className="roomname">
+              {room.name}
             </li>
           ))}
         </ul>
-				<form id="addRoomForm" action="" target="">
-    			<fieldset>
-						<legend>Create New Chat Room</legend>
-    				<input type="text" name="newRoomName" value="New Room Name" />
-    		    <input type="submit" value="+"/>
-					</fieldset>
-				</form>
+        <form id="addRoomForm" onSubmit={e => this.addNewRoom(e)}>
+          <fieldset>
+            <legend>Create New Chat Room</legend>
+            <input type="text" name="newRoomName" placeholder="New Room Name" onChange={e => this.handleChange(e)} />
+            <input type="submit" value="+" />
+          </fieldset>
+        </form>
       </section>
     );
   }
