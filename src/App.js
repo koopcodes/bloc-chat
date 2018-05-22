5b98c145635480aef6971486e9dafdd4d0f8b779
koopdev <koop@koop.codes>
2018-05-21 22:50:04 -0700

    User login but no Auth

--- src/App.js
+++ src/App.js
@@ -4,68 +4,61 @@ import "./App.css";
 import * as firebase from "firebase";
 import RoomList from "./components/RoomList.js";
 import MessageList from "./components/MessageList.js";
+import User from "./components/User.jsx";
 
 var config = {
-  apiKey: "AIzaSyAKGIi6kUifZBMmKS5C3b7zt0d9FirZO1k",
-  authDomain: "koop-chat.firebaseapp.com",
-  databaseURL: "https://koop-chat.firebaseio.com",
-  projectId: "koop-chat",
-  storageBucket: "koop-chat.appspot.com",
-  messagingSenderId: "337124092066",
+	apiKey: "AIzaSyAKGIi6kUifZBMmKS5C3b7zt0d9FirZO1k",
+	authDomain: "koop-chat.firebaseapp.com",
+	databaseURL: "https://koop-chat.firebaseio.com",
+	projectId: "koop-chat",
+	storageBucket: "koop-chat.appspot.com",
+	messagingSenderId: "337124092066",
 };
 firebase.initializeApp(config);
 
-/* var provider = new firebase.auth.GoogleAuthProvider();
-firebase.auth().signInWithRedirect(provider);
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-}); */
-
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { activeRoom: null };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			activeRoom: null,
+			username: null,
+		};
+	}
 
-  pickActiveRoom(room) {
+	pickActiveRoom(room) {
 		this.setState({ activeRoom: room });
-  }
+	}
 
-  render() {
+	setUser(user) {
+		this.setState({ username: user });
+	}
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Koop Chat</h1>
-        </header>
-        <aside id="sidebar">
-          <div id="logo" />
-          <div id="slogan">Chat Rooms</div>
-          <RoomList firebase={firebase} activeRoom={this.state.activeRoom} pickActiveRoom={this.pickActiveRoom.bind(this)} />
-          <nav />
-        </aside>
-        <span id="main">
-				<MessageList firebase={firebase} activeRoom={this.state.activeRoom} />
+	render() {
+		return (
+			<div className="App">
+				<nav id="main">
+					<User firebase={firebase} setUser={this.setUser.bind(this)} user={this.state.user} />
+				</nav>
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Koop Chat</h1>
+				</header>
+				<aside id="sidebar">
+					<div id="logo" />
+					<div id="slogan">Chat Rooms</div>
+					<RoomList
+						firebase={firebase}
+						activeRoom={this.state.activeRoom}
+						pickActiveRoom={this.pickActiveRoom.bind(this)}
+					/>
+					<nav />
+				</aside>
+				<span id="main">
+					<MessageList firebase={firebase} activeRoom={this.state.activeRoom} />
 				</span>
-      </div>
-    );
-  }
+			</div>
+		);
+	}
 }
 
 export default App;
